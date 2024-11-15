9084f989fc8f54e3a29116615ba65a5b14a7a390
A3hlee <ashley.senaeve@gmail.com>
2024-11-15 22:04:41 +0100

    Remove unused items

--- src/app/horeca/horeca.component.ts
+++ src/app/horeca/horeca.component.ts
@@ -1,24 +1,9 @@
 import { Component } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-horeca',
   templateUrl: './horeca.component.html',
   styleUrls: ['./horeca.component.css'],
-  providers: [NgbCarouselConfig]
 })
 export class HorecaComponent {
-  images = [
-    { title: 'First Slide', short: 'First Slide Short', src: "https://picsum.photos/id/700/900/500" },
-    { title: 'Second Slide', short: 'Second Slide Short', src: "https://picsum.photos/id/1011/900/500" },
-    { title: 'Third Slide', short: 'Third Slide Short', src: "https://picsum.photos/id/984/900/500" }
-  ];
-
-
-
-  constructor(config: NgbCarouselConfig) {
-    config.interval = 2000;
-    config.keyboard = true;
-    config.pauseOnHover = true;
-  }
 }
