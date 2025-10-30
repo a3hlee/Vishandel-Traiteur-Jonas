d51f15b72f7af41fe34648bddb1e221b6092b03e
A3hlee <ashley.senaeve@gmail.com>
2025-10-30 20:02:56 +0100

    Add vacation

--- src/app/vacation/vacation.component.ts
+++ src/app/vacation/vacation.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './vacation.component.css'
 })
 export class VacationComponent {
-  shortLeave = "3 t/m 9 november 2025";
+  shortLeave = "3 t/m 11 november 2025";
   yearlyLeave = "30 juni t/m 20 juli 2025";
 }
